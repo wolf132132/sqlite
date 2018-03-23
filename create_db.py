3091e1a5e1940e4896852d763e8638b3d60c9672
zirong wang <wolf132132@gmail.com>
2018-03-23 04:30:02 -0700

    sqlite-contact2

---
+++ create_db.py
@@ -0,0 +1,23 @@
+import sqlite3
+db = sqlite3.connect("contacts1.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts (name, phone, email) VALUES('Tim', 1234567, 'tim@email.com')")
+db.execute("INSERT INTO contacts VALUES('Brian', 1234, 'brian@myemail.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+# print(cursor.fetchall())
+
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+for name, phone, email in cursor:
+    print(name)
+    print(phone)
+    print(email)
+    print("-" * 20)
+
+cursor.close()
+db.close()
