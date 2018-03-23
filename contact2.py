840e690bd08123493fd6b0db47b844736c71222c
zirong wang <wolf132132@gmail.com>
2018-03-23 06:48:57 -0700

    sqlite-contact2

--- contact2.py
+++ contact2.py
@@ -2,6 +2,19 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
+new_email = "authorupdate@update.com"
+phone = input("Inout the phone number")
+
+update_sql = "UPDATE contacts SET email = '{}' WHERE phone = {}".format(new_email, phone)
+# update_sql = "UPDATE contacts SET email = 'authorupdate@update.com' WHERE phone = 1234"
+
+update_cursor = db.cursor()
+update_cursor.execute(update_sql)
+print("Are connections the same {}".format(update_cursor.connection == db))
+print()
+update_cursor.connection.commit()
+update_cursor.close()
+
 for name, phone, email in db.execute("SELECT * FROM contacts"):
     print(name)
     print(phone)
